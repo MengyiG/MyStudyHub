396b3ecaab580029f0b0ffcd579dd870d84fa23d
mengyi <yiii9292@gmail.com>
2023-07-30 21:32:41 -0400

    add URL and routing

--- MyStudyhub/urls.py
+++ MyStudyhub/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # whenever an empty string is requested, it will be redirected to base.urls
+    path('', include("base.urls")),
 ]
