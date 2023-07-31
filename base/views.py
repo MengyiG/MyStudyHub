bc70a935d5c3514e51ffe9e4ae2e62a7b9dd2e20
mengyi <yiii9292@gmail.com>
2023-07-31 10:48:12 -0400

    move template from root to app

--- base/views.py
+++ base/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
+rooms = [
+    {"id": 1, "name": "Let's learn Python"},
+    {"id": 2, "name": "Designing a Django app"},
+    {"id": 3, "name": "Frontend development"},
+]
+
 
 def home(request):
-    return render(request, "home.html")
+    context = {"rooms": rooms}
+    # use the render function to create an HTTP response
+    # containing the template home.html
+    # and pass the rooms variable to the template
+
+    # The purpose of this code is to handle an HTTP request
+    # fetch the appropriate template ("home.html"), and render it with the provided context data.
+    return render(request, "base/home.html", context)
 
 
 def room(request):
-    return render(request, "room.html")
+    return render(request, "base/room.html")
