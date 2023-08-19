5d6f7ebaf152c33fca880ac712b99417547396e9
mengyi <yiii9292@gmail.com>
2023-08-19 18:45:11 -0400

    query database and render user and topic to home

--- base/models.py
+++ base/models.py
@@ -1,15 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Room(models.Model):
-    #host=
-    #topic=
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(
+        Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    #participants = 
+    # participants =
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
+
+class Message(models.Model):
+    # build a one to many relationship between the User and Message models
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # build a one to many relationship between the Room and Message models
+    # if one room is deleted, all messages in that room will be deleted
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
