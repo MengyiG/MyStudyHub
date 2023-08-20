692af029069ebc36562d631f9d1865f6413800f5
mengyi <yiii9292@gmail.com>
2023-08-20 10:48:22 -0400

    delete room from db

--- base/urls.py
+++ base/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
+    path('delete-room/<str:pk>/', views.deteleRoom, name='delete-room'),
 ]
