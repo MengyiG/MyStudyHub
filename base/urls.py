e1d24bc8949f0157ed07a85ab4a10399f1e4baf8
mengyi <yiii9292@gmail.com>
2023-08-21 11:58:01 -0400

    user profile page

--- base/urls.py
+++ base/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     # (except for the / character, which separates path segments).
     # The matched string will be stored in the pk variable
     path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
 
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
