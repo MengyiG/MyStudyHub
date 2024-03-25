118e0a253da8cf2517276ab36d327669f5c53ef5
mengyi <yiii9292@gmail.com>
2024-03-25 13:01:00 -0400

    click more and show topics page

--- base/urls.py
+++ base/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('delete-room/<str:pk>/', views.deteleRoom, name='delete-room'),
     path('delete-message/<str:pk>/', views.deteleMessage, name='delete-message'),
     path('update-user/', views.updateUser, name='update-user'),
+    path('topics/', views.topicsPage, name='topics'),
 ]
